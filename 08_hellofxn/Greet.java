6afda3212e970b682330c14863d826864234b256
Josiah Moltz <josiahmoltz@Josiahs-MacBook-Pro.local>
2021-09-30 23:21:04 -0400

    initial setup: yes

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,12 +1,32 @@
+/*
+Josiah Moltz
+APCS
+HW08 -- Refactor Freshi Zero/Java Reviw/Making a method to greet peiople in Java
+2021--09--30
+*/
+
+/*
+DISCOVERIES
+-.gitignore tells git what files to ignore when pushing to the cloud
+-In .gitignore, an asterix infront of a file extension (like *.class) means that extension is ignored
+-public and static MUST BE INCLUDED in function definitions
+-ALL VARIABLES MUST BE NAMED!!
+-If a function is defined to take a parameter, that parameter must be included in the function call
+-class names MUST (letter for letter) match the file name
+
+UNRESOLVED QUESTIONS
+-What are public and static for??
+
+*/
+
 public class Greet {
 	public static void main(String[] args){
 		greet("Jacob");
 		greet("Amir");
 		greet("Josiah");
 	}
-	
+
 	public static void greet(String name) {
 		System.out.println("Josiah gives you a warm welcome, "+name);
 	}
-
 }
