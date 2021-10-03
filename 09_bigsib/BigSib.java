451c8aae4fcb0cddf0b0b35e585f87be8e4b668f
Josiah Moltz <josiahmoltz@Josiahs-MacBook-Pro.local>
2021-10-03 15:20:07 -0400

    initial setup

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,14 @@
+// Team With A Vowel: Lior (toothless), Alif (Grippy), Josiah (Hedwig)
+// APCS
+// HW09 -- What Are BigSibs Good For?/Calling methods from other classes/Calling the BigSib method greet from the Greet class
+// 2021-10-03
+
+public class BigSib{
+  public static void main(String[] args){
+    //foo
+  }
+
+  public static void greet(String name) {
+		System.out.println("Your Big Sib gives you a warm welcome, "+name);
+	}
+}
