1af10cd2dec0806c6b79ba3915239682cf567f44
Josiah Moltz <josiahmoltz@Josiahs-MacBook-Pro.local>
2021-10-04 19:57:12 -0400

    initial setup

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,4 +1,19 @@
+// Team With A Vowel: Lior (toothless), Alif (Grippy), Josiah (Hedwig)
+// APCS
+// HW10 -- Refactor Big Sib One/function types and return/Rewriting void BigSib.greet() to return a String
+// 2021-10-04
+
+/*
+DISCOVERIES
+-Tested out the idea that a java function needs a main only if it will be executable
+ (BigSib.greet(<name>) runs despite not having a main, and BigSib compiles without a main, but java BigSib throws an error)
+
+UNRESOLVED QUESTIONS
+*/
+
 public class BigSib {
+  //Purposefully no main method, demonstrating that a java file need not have a main to function (but still needs one to be executable)
+  //Upon executing, got run time error
   public static String greet(String name) {
     return "Your Big Sib gives you a warm welcome, " + name;
   }
--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,5 +1,21 @@
+// Team With A Vowel: Lior (toothless), Alif (Grippy), Josiah (Hedwig)
+// APCS
+// HW10 -- Refactor Big Sib One/function types and return/Rewriting void BigSib.greet() to return a String
+// 2021-10-04
+
+/*
+DISCOVERIES
+-Your function type (void, int, String, double, etc) dictates the type it is treated as.
+-You cannot print a void
+
+UNRESOLVED QUESTIONS
+-
+*/
+
 public class Greet {
   public static void main(String[] args) {
-    System.out.println(BigSib.greet("Josiah"));
+    System.out.println(BigSib.greet("Crosby"));
+    System.out.println(BigSib.greet("Stills"));
+    System.out.println(BigSib.greet("Nash"));
   }
 }
