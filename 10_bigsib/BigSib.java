a1870b0dc88f61c2a10385bd4d694aa0e993581e
Josiah Moltz <josiahmoltz@Josiahs-MacBook-Pro.local>
2021-10-04 20:03:45 -0400

    initial setup

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -3,14 +3,6 @@
 // HW10 -- Refactor Big Sib One/function types and return/Rewriting void BigSib.greet() to return a String
 // 2021-10-04
 
-/*
-DISCOVERIES
--Tested out the idea that a java function needs a main only if it will be executable
- (BigSib.greet(<name>) runs despite not having a main, and BigSib compiles without a main, but java BigSib throws an error)
-
-UNRESOLVED QUESTIONS
-*/
-
 public class BigSib {
   //Purposefully no main method, demonstrating that a java file need not have a main to function (but still needs one to be executable)
   //Upon executing, got run time error
--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -7,9 +7,16 @@
 DISCOVERIES
 -Your function type (void, int, String, double, etc) dictates the type it is treated as.
 -You cannot print a void
+-Tested out the idea that a java function needs a main only if it will be executable
+ (BigSib.greet(<name>) runs despite not having a main, and BigSib compiles without a main, but java BigSib throws a runtime error)
+-Git will push everything in the directory (not just the current one, but everything in the root directory (idk if thats correct notation))
+-***HYPOTHESIS*** object types are necessary to help compilation and creation of class files understand what variables are being used as inputs in what functions
+ Furthermore, the "tightness" of java is related to the fact that it is a compiled language
 
 UNRESOLVED QUESTIONS
--
+-Can a .class file be reverse engineered into the original .java file? (Not super related)
+-What exactly is a void type? Is it just no return?
+
 */
 
 public class Greet {
