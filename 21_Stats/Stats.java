c4e70eeee889f44a655469a4a30ba5b2daebaae5
Josiah-Moltz <jmoltz30@stuy.edu>
2021-10-20 12:00:05 -0400

    21

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -15,35 +15,45 @@ public class Stats {
   }
 
   public static double mean(double a, double b) {
-	
+	return (a + b) / 2.0;
   }
 
   public static int max(int a, int b) {
-
+	if (a <= b){
+		return b;
+	}
+	else{
+		return a;
+	}
   }
 
   public static double max(double a, double b) {
-
+	if (a <= b){
+		return b;
+	}
+	else{
+		return a;
+	}
   }
 
   public static int geoMean(int a, int b) {
-
+	return Math.sqrt(a * b);
   }
 
   public static double geoMean(double a, double b) {
-
+	return Math.sqrt(a * b);
   }
 
   public static int max(int a, int b, int c) {
-
+	return max(max(a, b), c);	// that's cheating - BW
   }
 
   public static double max(double a, double b, double c) {
-
+	return max(max(a, b), c); 
   }
 
   public static int geoMean(int a, int b, int c) {
-
+	return Math.cbrt(a * b * c);
   }
 
   public static double geoMean(double a, double b, double c) {
