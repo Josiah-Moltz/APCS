9a150748499a466dc148f9fb5a523178e12fa96d
Josiah-Moltz <jmoltz30@stuy.edu>
2022-02-08 20:20:52 -0500

    61

--- 61_mergesort/MergeSort.java
+++ 61_mergesort/MergeSort.java
@@ -1,8 +1,17 @@
+// MNM: Mosiah, Nora Miller
+// APCS pd6
+// HW61: Instructions so Simple...
+// 2022-02-07
+// time spent: .8 hrs
+
 /***
   class MergeSort
   Implements mergesort on array of ints.
 
   Summary of Algorithm:
+  0. Split array in a binary fashion until subarrays are 1 element long
+  1. Merge pairs of most recently split subarrays until the entire array
+    has been merged together. (See FIRMWARE for a description of merge)
 
   ***/
 
@@ -17,21 +26,29 @@ public class MergeSort
    ******************************************************/
   private static int[] merge( int[] a, int[] b )
   {
-    int[] result = new int[ a.length + b.length ];
-    int aIndex = 0;
-    int bIndex = 0;
+    int[] result = new int[ a.length + b.length ];  // initialize result
+    //printArray(result);
+    int aIndex = 0; // initialize aIndex, keeps track of the next element to be looked at in a
+    int bIndex = 0; // initialize aIndex, keeps track of the next element to be looked at in b
 
-    for (int i = 0; i < result.length; i++) {
-      if ( a[aIndex] < b[bIndex] ){
-        result[i] = a[aIndex];
+    while (aIndex < a.length && bIndex < b.length) {  // runs until either a or b is exhausted
+      if (a[aIndex] < b[bIndex]) {  // take smallest element and place it into result
+        result[aIndex + bIndex] = a[aIndex];
         aIndex += 1;
       }
-      else {
-        result[i] = b[bIndex];
+      else {  // take smallest element and place it into result
+        result[aIndex + bIndex] = b[bIndex];
         bIndex += 1;
       }
     }
-
+    while (bIndex < b.length) { // takes care of leftovers in b
+      result[aIndex + bIndex] = b[bIndex];
+      bIndex += 1;
+    }
+    while (aIndex < a.length) { // takes care of leftovers in a
+      result[aIndex + bIndex] = a[aIndex];
+      aIndex += 1;
+    }
     return result;
   }//end merge()
 
@@ -41,10 +58,25 @@ public class MergeSort
    * Sorts input array using mergesort algorithm
    * Returns sorted version of input array (ascending)
    ******************************************************/
-  // public static int[] sort( int[] arr )
-  // {
-  //
-  // }//end sort()
+  public static int[] sort( int[] arr )
+  {
+    if (arr.length == 1) {
+      return arr; // basecase
+    }
+    else {
+      int[] a = new int[arr.length / 2];  // initialize lower "half" or arr
+      int[] b = new int[arr.length - arr.length / 2]; // initalize upper "half" of arr
+
+      for (int i = 0; i < arr.length / 2; i++) {
+        a[i] = arr[i];  // fill lower "half"
+      }
+      for (int j = 0; j < arr.length - arr.length / 2; j++) {
+        b[j] = arr[j + arr.length / 2]; // fill upper "half"
+      }
+
+      return merge( sort(a), sort(b));  // recursive case
+    }
+  }//end sort()
 
 
 
@@ -85,7 +117,10 @@ public class MergeSort
 
       System.out.println("\nMerging arr1 and arr0: ");
       printArray( merge(arr1,arr0) );
-    /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
+
+      System.out.println("\nMerging arr2 and arr3: ");
+      printArray( merge(arr2,arr3) );
+
       System.out.println("\nMerging arr4 and arr6: ");
       printArray( merge(arr4,arr6) );
 
@@ -94,7 +129,6 @@ public class MergeSort
       printArray( sort( arr5 ) );
       printArray( sort( arr6 ) );
       printArray( sort( arr7 ) );
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }//end main()
 
 }//end class MergeSort
