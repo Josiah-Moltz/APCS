0a05141cc3c478682ef2c890e8a7bda39d76ae32
Josiah-Moltz <jmoltz30@stuy.edu>
2022-02-15 06:51:20 -0500

    HW64

--- cb/recursion-1/ChangeXY.java
+++ cb/recursion-1/ChangeXY.java
@@ -1,16 +1,28 @@
-// Junior: Josiah Moltz, Raven Tang
-// APCS pd6
-// HW64: Revisitation
-// 2022-02-14
-// time spent: 1 hrs
+/*
+Junior: Josiah Moltz, Raven (Ruiwen) Tang
+APCS pd06
+HW64 -- Revisitation / completing CodingBat Recursion-1 and Recursion-2
+2022-02-14
+time spent: 02.0 hrs
+*/
 
-// tree-y
-public String changeXY(String str) {
-  if (str.equals("x")) {
-    return "y";
-  }
-  if (str.length() <= 1) {
-    return str;
-  }
-  return changeXY( str.substring(0,str.length()/2) ) + changeXY( str.substring(str.length()/2) );
+public class ChangeXY{
+    /*
+    Given a string, compute recursively (no loops) a new string where all the lowercase 'x' chars have been changed to 'y' chars.
+    */
+    // tree-y
+    public static String changeXY(String str) {
+        if (str.equals("x")) {
+        return "y";
+        }
+        if (str.length() <= 1) {
+        return str;
+        }
+        return changeXY( str.substring(0,str.length()/2) ) + changeXY( str.substring(str.length()/2) );
+    }
+    public static void main(String[] args){
+        System.out.println(changeXY("codex") + "...should be codey");
+        System.out.println(changeXY("xxhixx") + "...should be yyhiyy");
+        System.out.println(changeXY("xhihix") + "...should be yhiyhiy");
+    }
 }
--- cb/recursion-1/Factorial.java
+++ cb/recursion-1/Factorial.java
@@ -1,14 +1,24 @@
-// Junior: Josiah Moltz, Raven Tang
-// APCS pd6
-// HW64: Revisitation
-// 2022-02-14
-// time spent: 1 hrs
+/*
+Junior: Josiah Moltz, Raven (Ruiwen) Tang
+APCS pd06
+HW64 -- Revisitation / completing CodingBat Recursion-1 and Recursion-2
+2022-02-14
+time spent: 02.0 hrs
+*/
 
-public int factorial(int n) {
-  if ( n == 1 ) {
-    return 1;
-  }
-  else {
-    return n * factorial( n-1 );
-  }
+public class Factorial{
+    /*
+    Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+    */
+    public static int factorial(int n) {
+        if(n == 1){
+          return 1;
+        }
+        return n * factorial(n-1);
+    }
+    public static void main(String[] args){
+        System.out.println(factorial(1) + "...should be 1");
+        System.out.println(factorial(2) + "...should be 2");
+        System.out.println(factorial(3) + "...should be 6");
+    }
 }
