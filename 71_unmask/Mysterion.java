c2f8636530149d34dd7a6b3d2bd1be5f77191c36
Josiah-Moltz <jmoltz30@stuy.edu>
2022-03-08 22:51:40 -0500

    72

--- 71_unmask/Mysterion.java
+++ 71_unmask/Mysterion.java
@@ -60,43 +60,55 @@ partition (c): index of v
   public static void main(String[] args) {
     int[] arr0 = {7, 1, 5, 12, 3};
     partitionator(arr0, 0, 4, 2);
-    print(arr0); // {1, 3, 5, 12, 7} everything before 5 is <5, everything after 5 is >5
+    //print(arr0); // {1, 3, 5, 12, 7} everything before 5 is <5, everything after 5 is >5
 
     int[] arr1 = {1, 3, 5, 7, 12};
     partitionator(arr1, 0, 4, 2);
-    print(arr1); // {1, 3, 5, 7, 12} everything before 5 is <5, everything after 5 is >5
+    //print(arr1); // {1, 3, 5, 7, 12} everything before 5 is <5, everything after 5 is >5
 
     int[] arr2 = {12, 7, 5, 3, 1};
     partitionator(arr2, 0, 4, 2);
-    print(arr2); // {1, 3, 5, 7, 12} everything before 5 is <5, everything after 5 is >5
+    //print(arr2); // {1, 3, 5, 7, 12} everything before 5 is <5, everything after 5 is >5
 
     int[] arr3 = {7, 1, 5, 12, 3};
     partitionator(arr3, 0, 4, 3);
-    print(arr3); // {7, 1, 5, 3, 12} everything before 12 is <12, everything after 12 is >12
+    //print(arr3); // {7, 1, 5, 3, 12} everything before 12 is <12, everything after 12 is >12
 
     int[] arr4 = {7, 1, 12, 5, 3};
     partitionator(arr4, 0, 4, 2);
-    print(arr4); // {7, 1, 3, 5, 12} everything before 12 is <12, everything after 12 is >12
+    //print(arr4); // {7, 1, 3, 5, 12} everything before 12 is <12, everything after 12 is >12
 
     int[] arr5 = {7, 1, 12, 5, 3};
     partitionator(arr5, 0, 4, 3);
-    print(arr5); // {7, 1, 3, 5, 12} everything before 5 is <5, everything after 5 is >5
+    //print(arr5); // {7, 1, 3, 5, 12} everything before 5 is <5, everything after 5 is >5
 
     int[] arr6 = {7, 12, 1, 5, 3};
     partitionator(arr6, 0, 4, 2);
-    print(arr6); // {1, 12, 3, 5, 7} everything before 1 is <1, everything after 1 is >1
+    //print(arr6); // {1, 12, 3, 5, 7} everything before 1 is <1, everything after 1 is >1
 
     int[] arr7 = {7, 12, 1, 5, 3};
     partitionator(arr7, 3, 4, 3);
-    print(arr7); // {7, 12, 1, 3, 5} elements at index 3 up to 5 are less than 5
+    //print(arr7); // {7, 12, 1, 3, 5} elements at index 3 up to 5 are less than 5
 
     int[] arr8 = {2, 7, 5, 1, 3};
     partitionator(arr8, 3, 4, 4);
-    print(arr8); // {2, 7, 5, 1, 3} elements at index 4 up to 5 are less than 4
+    //print(arr8); // {2, 7, 5, 1, 3} elements at index 4 up to 5 are less than 4
 
     int[] arr9 = {2, 42, 3, 5, 91, 34, 88, 22, 153};
     partitionator(arr9, 0, 8, 4);
     print(arr9); // {2, 42, 3, 5, 34, 88, 22, 91, 153}
+
+    int[] arr10 = {2, 42, 3, 5, 91, 34, 88, 22, 153};
+    partitionator(arr10, 0, 8, 2);
+    print(arr10);
+
+    int[] arr11 = {2, 42, 3, 5, 91, 34, 88, 22, 153};
+    partitionator(arr11, 0, 8, 5);
+    print(arr11);
+
+    int[] arr12 = {2, 42, 3, 5, 91, 34, 88, 22, 153};
+    partitionator(arr12, 0, 8, 8);
+    print(arr12);
   }
 
 }
