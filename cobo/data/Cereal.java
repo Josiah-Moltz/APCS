62ff09e8176ecaed815a0c074fd7e282f003b31f
Josiah-Moltz <jmoltz30@stuy.edu>
2022-04-06 12:02:48 -0400

    Cereal+sinbad

---
+++ cobo/data/Cereal.java
@@ -0,0 +1,92 @@
+public class Cereal {
+  private String name, type;
+  private int calories, protein, fat, sodium, fiber, carbohydrates, sugar, potassium, vitamins, shelf;
+  private float weight, cups, rating;
+
+  public Cereal( String nickname, String nicktype,
+                 int nickcalories, int nickprotein, int nickfat, int nicksodium, int nickfiber,
+                 int nickcarbohydrates, int nicksugar, int nickpotassium, int nickvitamins, int nickshelf,
+                 float nickweight, float nickcups, float nickrating
+               )
+  {
+    name = nickname;
+    type = nicktype;
+    calories = nickcalories;
+    protein = nickprotein;
+    fat = nickfat;
+    sodium = nicksodium;
+    fiber = nickfiber;
+    carbohydrates = nickcarbohydrates;
+    sugar = nicksugar;
+    potassium = nickpotassium;
+    vitamins = nickvitamins;
+    shelf = nickshelf;
+    weight = nickweight;
+    cups = nickcups;
+    rating = nickrating;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public int getCalories() {
+    return calories;
+  }
+
+  public int getProtein() {
+    return protein;
+  }
+
+  public int getFat() {
+    return fat;
+  }
+
+  public int getSodium() {
+    return sodium;
+  }
+
+  public int getFiber() {
+    return fiber;
+  }
+
+  public int getCarbohydrates() {
+    return carbohydrates;
+  }
+
+  public int getSugar() {
+    return sugar;
+  }
+
+  public int getPotassium() {
+    return potassium;
+  }
+
+  public int getVitamins() {
+    return vitamins;
+  }
+
+  public int getShelf() {
+    return shelf;
+  }
+
+  public float getWeight() {
+    return weight;
+  }
+
+  public float getCups() {
+    return cups;
+  }
+
+  public float getRating() {
+    return rating;
+  }
+
+  public String toString() {
+    String result = FIX LATERRRRRR
+  }
+}
